4ee7f3ad94085434e1c9bbfb4bd4eb137818d5b0
NoeOnDev <alxg5516@gmail.com>
2024-08-09 00:45:00 -0600

    feat: Update file paths and imports for App component and related files

---
+++ src/components/screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styled from 'styled-components/native';
+
+export const HomeScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
---
+++ src/containers/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {HomeScreen} from '../components/screens/HomeScreen';
+
+const App: React.FC = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
